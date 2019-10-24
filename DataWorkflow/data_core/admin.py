7134b49186ba5b10cdf3435f6c04b3cc23b17cc4
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-10-24 11:13:03 +0100

    Added admin for existing models.

--- DataWorkflow/data_core/admin.py
+++ DataWorkflow/data_core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+import data_core.models
 
-# Register your models here.
+
+class EndpointAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_by', 'created_on', 'modified_by', 'modified_on', )
+    ordering = ['name', 'created_by', 'created_on', 'modified_by', 'modified_on', ]
+
+
+class BucketAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'name', 'status', 'endpoint', 'created_by', 'created_on', 'modified_by', 'modified_on', )
+    ordering = ['friendly_name', 'name', 'status', 'endpoint', 'created_by', 'created_on', 'modified_by', 'modified_on', ]
+
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('bucket', 'object_storage_key', 'md5', 'size', 'created_by', 'created_on', 'modified_by', 'modified_on', )
+    ordering = ['bucket', 'object_storage_key', 'md5', 'size', 'created_by', 'created_on', 'modified_by', 'modified_on', ]
+
+
+admin.site.register(data_core.models.Endpoint, EndpointAdmin)
+admin.site.register(data_core.models.Bucket, BucketAdmin)
+admin.site.register(data_core.models.File, FileAdmin)
