c445d126db3e4f6f5cea522f46f3a43acafb6a5d
Carles Pina i Estany <carles@pina.cat>
2019-10-30 13:16:54 +0000

    Fixes deployment in Docker

--- tools/backup-database.sh
+++ tools/backup-database.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ ! -n "$1" ]
 then
-	echo "Syntax error"
-	echo $0: usage: $0 output_directory
-	echo 'The name of the file will be dataworkflow-$DATE-$USER.sql'
-	echo
+	echo "Please provide directory to export the database to"
 	exit 1
 fi
 
-DIRECTORY="$1"
 
-USER=$(whoami)
-OUTPUT_NAME=$(date "+dataworkflow-%Y%m%d-%H%M%S-$USER.sql")
-OUTPUT_FILE="$DIRECTORY/$OUTPUT_NAME"
+if [ ! -d "$1" ]
+then
+	echo "$1 needs to be a directory"
+	exit 1
+fi
 
-mysqldump --defaults-file=/etc/mysql/data-workflow.cnf dataworkflow > "$OUTPUT_FILE"
+if [ ! -n "$2" ]
+then
+	echo "Please provide rclone.conf file (mandatory, to upload the file somewhere)"
+	exit 1
+fi
 
-echo
-echo "Backup done: $OUTPUT_FILE"
+if [ ! -n "$3" ]
+then
+	echo "Please provide the dest:path in rclone"
+	exit 1
+fi
+
+OUTPUT_DIRECTORY="$1"
+RCLONE_CONFIG="$2"
+RCLONE_DEST_PATH="$3"
+
+DATETIME="$(date +%Y%m%d-%H%M%S)"
+
+OUTPUT_FILE="$OUTPUT_DIRECTORY/data-workflow-$DATETIME.sql.gz"
+docker exec data-workflow_data-workflow_1 mysqldump --defaults-file=/var/run/secrets/data_workflow_mysql.conf data_workflow | gzip > "$OUTPUT_FILE"
+echo "$OUTPUT_FILE"
+
+rclone --config="$RCLONE_CONFIG" copy "$OUTPUT_FILE" "$RCLONE_DEST_PATH"
