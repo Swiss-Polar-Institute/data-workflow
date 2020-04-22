5585978744b20b27eae58d78aa5b1fc48fbfe041
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-22 18:35:52 +0200

    Changed backup script to work for local database (removed docker parts).

--- tools/backup-database.sh
+++ tools/backup-database.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 
-
-if [ ! -n "$1" ]
+if [ $# -ne 1 ]
 then
-	echo "Please provide directory to export the database to"
+	echo "Syntax error"
+	echo $0: usage: $0 output_directory
+	echo 'The name of the file will be dataworkflow-$DATE-$USER.sql'
+	echo
 	exit 1
 fi
 
+DIRECTORY="$1"
 
-if [ ! -d "$1" ]
-then
-	echo "$1 needs to be a directory"
-	exit 1
-fi
+USER=$(whoami)
+OUTPUT_NAME=$(date "+dataworkflow-%Y%m%d-%H%M%S-$USER.sql")
+OUTPUT_FILE="$DIRECTORY/$OUTPUT_NAME"
 
-if [ ! -n "$2" ]
-then
-	echo "Please provide rclone.conf file (mandatory, to upload the file somewhere)"
-	exit 1
-fi
+mysqldump --defaults-file=/etc/mysql/data_workflow_mysql.conf dataworkflow > "$OUTPUT_FILE"
 
-if [ ! -n "$3" ]
-then
-	echo "Please provide the dest:path in rclone"
-	exit 1
-fi
-
-OUTPUT_DIRECTORY="$1"
-RCLONE_CONFIG="$2"
-RCLONE_DEST_PATH="$3"
-
-DATETIME="$(date +%Y%m%d-%H%M%S)"
-
-OUTPUT_FILE="$OUTPUT_DIRECTORY/data-workflow-$DATETIME.sql.gz"
-docker exec data-workflow_data-workflow_1 mysqldump --defaults-file=/var/run/secrets/data_workflow_mysql.conf data_workflow | gzip > "$OUTPUT_FILE"
-echo "$OUTPUT_FILE"
-
-rclone --config="$RCLONE_CONFIG" copy "$OUTPUT_FILE" "$RCLONE_DEST_PATH"
+echo
+echo "Backup done: $OUTPUT_FILE"
