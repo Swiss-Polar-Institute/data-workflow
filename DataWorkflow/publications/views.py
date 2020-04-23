0ea2f4578888dab3e6194293c13a0e4eea11eb68
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-23 15:25:46 +0200

    Added publications to publication_list template.

--- DataWorkflow/publications/views.py
+++ DataWorkflow/publications/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Publication
+
 
 # Create your views here.
 
 def publication_list(request):
-    return render(request, 'publications/publication_list.html', {})
+
+    publications = Publication.objects.all()
+
+    return render(request, 'publications/publication_list.html', {'publications': publications})
