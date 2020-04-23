7d6e069650cc07ca36020cc1748076915a3917e2
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-23 14:49:48 +0200

    Added initial part of publication form.

--- DataWorkflow/publications/forms.py
+++ DataWorkflow/publications/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 
 from .models import Publication
+
+class PublicationForm(forms.ModelForm):
+
+    class Meta:
+        model = Publication
+        fields = ('identifier', 'publisher', 'publication_year', 'resource_type', 'version')
