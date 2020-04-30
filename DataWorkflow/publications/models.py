660303d9f67d5eebfc1d9abac51390230f39dabe
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-30 12:25:32 +0200

    Refactored models. Moved creator to publication model as manytomany.

--- DataWorkflow/publications/models.py
+++ DataWorkflow/publications/models.py
@@ -66,6 +66,133 @@ class ResourceType(models.Model):
         unique_together = (('description', 'type_general'),)
 
 
+class AbstractIdentifier(models.Model):
+    """
+    Uniquely identifies an item according to various schemas.
+    """
+    identifier = models.CharField(max_length=200,
+                                  help_text='Uniquely identifies an item according to various schemas.', blank=False,
+                                  null=False)
+    identifier_schema = models.CharField(max_length=200, help_text='Name of the identifier schema.',
+                                         blank=False, null=False)
+    schema_uri = models.URLField(max_length=100, help_text='URI of the identifier schema.', blank=False,
+                                 null=False)  # conditions here are required for the AffiliationIdentifier. They could
+
+    # be relaxed for the NameIdentifier.
+
+    class Meta:
+        abstract = True
+        unique_together = (('identifier', 'identifier_schema'),)
+
+
+class AffiliationIdentifier(AbstractIdentifier):
+    """
+    Uniquely identifies the organisational affiliation of the creator
+    """
+
+    def __str__(self):
+        return "{} {}".format(self.identifier_schema, self.identifier)
+
+
+class Affiliation(models.Model):
+    """
+    Organisational or institutional affiliation of the creator.
+    """
+    name = models.CharField(max_length=200, help_text='Name of the affiliation. If a research group name has already '
+                                                      'been used for the creator, then the organisation to which this'
+                                                      'group formally belongs could be used here.', blank=False,
+                            null=False)
+    identifier = models.OneToOneField(AffiliationIdentifier, max_length=200,
+                                      help_text='Uniquely identifies the organisational affiliation of the creator.',
+                                      blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+    class Meta:
+        unique_together = (('name', 'identifier'),)
+
+
+class NameIdentifier(AbstractIdentifier):
+    """
+    Uniquely identifies an individual or organisation according to various schemas.
+    """
+
+    def __str__(self):
+        return "{} {}".format(self.identifier_schema, self.identifier)
+
+
+class NameType(models.Model):
+    """
+    Type of name.
+    """
+    name = models.CharField(max_length=50, help_text='Type of name', blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class CreatorName(models.Model):
+    """
+    Full name of the creator.
+    """
+    name = models.CharField(max_length=100,
+                            help_text='Full name of the creator. For a personal name, the format should be family name,'
+                                      'given name. Non-roman names may be transliterated according to ALA-LC schemas.',
+                            blank=False, null=False)
+    type = models.ForeignKey(NameType, help_text='The type of name.', blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+    class Meta:
+        unique_together = (('name', 'type'),)
+
+
+class Creator(models.Model):
+    """
+    Main researchers involved in the publication, or authors of the publication, in order of priority. This can be an
+    organisation or a person.
+    """
+    name = models.OneToOneField(CreatorName, help_text='Creator of the publication.', blank=False, null=False,
+                                on_delete=models.PROTECT)
+    given_name = models.CharField(max_length=50, help_text='Personal or first name of the creator.', blank=True,
+                                  null=True)
+    family_name = models.CharField(max_length=50, help_text='Surname or last name of the creator.', blank=True,
+                                   null=True)
+    name_identifier = models.ForeignKey(NameIdentifier,
+                                        help_text='Uniquely identifies an individual or organisation according to '
+                                                  'various schemas.',
+                                        blank=True, null=True, on_delete=models.PROTECT)
+    affiliation = models.ForeignKey(Affiliation,
+                                    help_text='Organisational or institutional affiliation of the creator.', blank=True,
+                                    null=True, on_delete=models.PROTECT)
+
+    # def __str__(self):
+    #     publications = self.publication.all()
+    #     publication_identifiers = []
+    #
+    #     for publication in publications:
+    #         publication_identifiers.append(publication.identifier.uri)
+    #
+    #     publication_str = ";".join(publication_identifiers)
+    #
+    #     return "{} {}. {}. Publications:{}".format(self.given_name, self.family_name, self.affiliation, publication_str)
+    #
+    # def main_name_identifiers(self):
+    #     main_name_identifiers = NameIdentifier.objects.filter(creator=self)
+    #
+    #     return main_name_identifiers
+    #
+    # def main_affiliations(self):
+    #     main_affiliations = Affiliation.objects.filter(creator=self)
+    #
+    #     return main_affiliations
+
+    class Meta:
+        unique_together = (('name', 'affiliation'),)
+
+
 class Publication(models.Model):
     """
     Describes a publication according to the DataCite Metadata Schema version 4.3.
@@ -75,6 +202,10 @@ class Publication(models.Model):
                                       help_text='Unique identifier that identifies a resource. This can relate to a '
                                                 'specific version or all versions.', blank=False, null=False,
                                       on_delete=models.PROTECT, unique=True)
+    creator = models.ManyToManyField(Creator,
+                                     help_text='Main researchers involved in the publication, or authors of the '
+                                               'publication, in order of priority. This can be an organisation or a '
+                                               'person.', blank=False)
     publisher = models.CharField(max_length=500, help_text='The name of the entity that holds, archives, publishes '
                                                            'prints, distributes, releases, issues, or produces the '
                                                            'resource. This property will be used to formulate the '
@@ -110,7 +241,7 @@ class Publication(models.Model):
             super().save(*args, **kwargs)
 
             Title.objects.update_or_create(
-                publication=self, type=getattr(self, 'title_type'), defaults={'name':getattr(self, 'title')}
+                publication=self, type=getattr(self, 'title_type'), defaults={'name': getattr(self, 'title')}
             )
 
     def main_title(self):
@@ -129,7 +260,8 @@ class Publication(models.Model):
         return main_creators
 
     def __str__(self):
-        return "{} ({}). Version: {}. {}. {}.".format(self.main_title(), self.publication_year, self.version, self.publisher, self.identifier)
+        return "{} ({}). Version: {}. {}. {}.".format(self.main_title(), self.publication_year, self.version,
+                                                      self.publisher, self.identifier)
 
 
 class Size(models.Model):
@@ -326,137 +458,19 @@ class Date(models.Model):
         unique_together = (('publication', 'date', 'type'),)
 
 
-class NameType(models.Model):
-    """
-    Type of name.
-    """
-    name = models.CharField(max_length=50, help_text='Type of name', blank=False, null=False, unique=True)
-
-    def __str__(self):
-        return "{}".format(self.name)
 
 
-class CreatorName(models.Model):
-    """
-    Full name of the creator.
-    """
-    name = models.CharField(max_length=100,
-                            help_text='Full name of the creator. For a personal name, the format should be family name,'
-                                      'given name. Non-roman names may be transliterated according to ALA-LC schemas.',
-                            blank=False, null=False)
-    type = models.ForeignKey(NameType, help_text='The type of name.', blank=True, null=True, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return "{}".format(self.name)
-
-    class Meta:
-        unique_together = (('name', 'type'),)
 
 
-class AbstractIdentifier(models.Model):
-    """
-    Uniquely identifies an item according to various schemas.
-    """
-    identifier = models.CharField(max_length=200,
-                                  help_text='Uniquely identifies an item according to various schemas.', blank=False,
-                                  null=False)
-    identifier_schema = models.CharField(max_length=200, help_text='Name of the identifier schema.',
-                                         blank=False, null=False)
-    schema_uri = models.URLField(max_length=100, help_text='URI of the identifier schema.', blank=False,
-                                 null=False)  # conditions here are required for the AffiliationIdentifier. They could
-
-    # be relaxed for the NameIdentifier.
-
-    class Meta:
-        abstract = True
-        unique_together = (('identifier', 'identifier_schema'),)
 
 
-class NameIdentifier(AbstractIdentifier):
-    """
-    Uniquely identifies an individual or organisation according to various schemas.
-    """
-
-    def __str__(self):
-        return "{} {}".format(self.identifier_schema, self.identifier)
 
 
-class AffiliationIdentifier(AbstractIdentifier):
-    """
-    Uniquely identifies the organisational affiliation of the creator
-    """
-
-    def __str__(self):
-        return "{} {}".format(self.identifier_schema, self.identifier)
 
 
-class Affiliation(models.Model):
-    """
-    Organisational or institutional affiliation of the creator.
-    """
-    name = models.CharField(max_length=200, help_text='Name of the affiliation. If a research group name has already '
-                                                      'been used for the creator, then the organisation to which this'
-                                                      'group formally belongs could be used here.', blank=False,
-                            null=False)
-    identifier = models.OneToOneField(AffiliationIdentifier, max_length=200,
-                                      help_text='Uniquely identifies the organisational affiliation of the creator.',
-                                      blank=True, null=True, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return "{}".format(self.name)
-
-    class Meta:
-        unique_together = (('name', 'identifier'),)
 
 
-class Creator(models.Model):
-    """
-    Main researchers involved in the publication, or authors of the publication, in order of priority. This can be an
-    organisation or a person.
-    """
-    publication = models.ManyToManyField(Publication,
-                                    help_text='Publication in which this creator has been involved or authored.',
-                                    blank=False)
-    name = models.OneToOneField(CreatorName, help_text='Creator of the publication.', blank=False, null=False,
-                                on_delete=models.PROTECT)
-    given_name = models.CharField(max_length=50, help_text='Personal or first name of the creator.', blank=True,
-                                  null=True)
-    family_name = models.CharField(max_length=50, help_text='Surname or last name of the creator.', blank=True,
-                                   null=True)
-    name_identifier = models.ForeignKey(NameIdentifier,
-                                        help_text='Uniquely identifies an individual or organisation according to '
-                                                  'various schemas.',
-                                        blank=True, null=True, on_delete=models.PROTECT)
-    affiliation = models.ForeignKey(Affiliation,
-                                    help_text='Organisational or institutional affiliation of the creator.', blank=True,
-                                    null=True, on_delete=models.PROTECT)
 
-    def __str__(self):
-        publications = self.publication.all()
-        publication_identifiers = []
-
-        for publication in publications:
-            publication_identifiers.append(publication.identifier.uri)
-
-        publication_str = ";".join(publication_identifiers)
-
-        return "{} {}. {}. Publications:{}".format(self.given_name, self.family_name, self.affiliation, publication_str)
-
-    def main_name_identifiers(self):
-
-        main_name_identifiers = NameIdentifier.objects.filter(creator=self)
-
-        return main_name_identifiers
-
-    def main_affiliations(self):
-
-        main_affiliations = Affiliation.objects.filter(creator=self)
-
-        return main_affiliations
-
-
-    class Meta:
-        unique_together = (('name', 'affiliation'),)
 
 
 class TitleType(models.Model):
